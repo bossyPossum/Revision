43331756d2b95548b371619a9111229dd6f87e19
Vivian Wan <vivianpswan@gmail.com>
2020-07-05 01:24:18 +1000

    running hello world again

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require ('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => res.send('hello world and hello to you Rubz'))
 
 app.listen(port, () => console.log(`your app is listening at http://localhost:${port}.`))
