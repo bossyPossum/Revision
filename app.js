aab7e8304e0cb6f57edb02a2e52f81d38fbfe246
Vivian Wan <vivianpswan@gmail.com>
2020-07-05 23:58:04 +1000

    req.query

--- app.js
+++ app.js
@@ -17,8 +17,12 @@ app.get('/',(req, res) => res.send('happy sunday'))
 // })
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3])
+    res.send([1, 2, 3]);
 })
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.query);
+});
 
 // app.post('/user', (req, res) => {
 //     res.send('I got a post request')
