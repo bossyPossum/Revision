78dac2a1bd7ccc9d20936dfcf8b9b23d20d832a0
Vivian Wan <vivianpswan@gmail.com>
2020-07-05 23:40:14 +1000

    added proper PORT

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-const express = require ('express')
+// const express = require ('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => res.send('hello world and hello to you Rubz'))
+
+// app.listen(port, () => console.log(`your app is listening at http://localhost:${port}.`))
+
+const express = require ('express') //ES6 = import express from 'express'
 const app = express()
-const port = 3000
+// const port = 3001
 
-app.get('/', (req, res) => res.send('hello world and hello to you Rubz'))
+app.get('/',(req, res) => res.send('happy sunday'))
 
-app.listen(port, () => console.log(`your app is listening at http://localhost:${port}.`))
+// app.get('/', (req, res) => {
+//     res.send(JSON.stringify('happy happy sundday RUBZ'))
+// })
+
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3])
+})
+
+// app.post('/user', (req, res) => {
+//     res.send('I got a post request')
+// })
+
+//PORT
+const port = process.env.PORT || 3001
+app.listen(port, () => console.log (`I am on port ${port}.`))
