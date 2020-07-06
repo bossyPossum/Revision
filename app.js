b291730cac88174259011cb09815f83fa1f401bf
Vivian Wan <vivianpswan@gmail.com>
2020-07-06 18:10:51 +1000

    added POST request

--- app.js
+++ app.js
@@ -7,8 +7,10 @@
 // app.listen(port, () => console.log(`your app is listening at http://localhost:${port}.`))
 
 const express = require ('express') //ES6 = import express from 'express'
-const app = express()
-// const port = 3001
+const app = express();
+
+app.use(express.json()); //middleware
+
 const courses = [
     { id: 1, name: 'course 1'},
     { id: 2, name: 'course 2'},
@@ -18,14 +20,28 @@ const courses = [
 app.get('/',(req, res) => res.send('happy sunday'))
 
 // app.get('/', (req, res) => {
-//     res.send(JSON.stringify('happy happy sundday RUBZ'))
+//     res.send(JSON.stringify('Hello World'))
 // })
+
+
 
 app.get('/api/courses', (req, res) => {
     res.send(courses);
 })
+
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.query)
+   const course = courses.find(c => c.id === parseInt(req.params.id)); // req.params.id will return a string
+   if (!course) res.status(404).send('the course is not found');
+   res.send(course)
+})
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: course.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
 })
 
 // app.get('/api/posts/:year/:month', (req, res) => {
